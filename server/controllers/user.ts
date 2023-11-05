570b08cc7e17ac4c63bc7e4f424c2ef71eaf8779
Ian Lai <a186235@gmail.com>
2023-11-05 18:33:13 +0800

    fix: change app_id

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -85,16 +85,17 @@ export async function signIn(req: Request, res: Response) {
 
 async function isFbTokenValid(userToken: string) {
   console.log(FB_APP_ID, FB_APP_SECRET);
-  console.log(`
+  // const response = await axios.get(`
+  //   https://graph.facebook.com/debug_token?
+  //   input_token=${userToken}
+  //   &access_token=${FB_APP_ID}|${FB_APP_SECRET}
+  // `);
+
+  const response = await axios.get(`
   https://graph.facebook.com/debug_token?
   input_token=${userToken}
-  &access_token=${FB_APP_ID}|${FB_APP_SECRET}
+  &access_token=${FB_APP_ID}
 `);
-  const response = await axios.get(`
-    https://graph.facebook.com/debug_token?
-    input_token=${userToken}
-    &access_token=${FB_APP_ID}|${FB_APP_SECRET}
-  `);
   return response?.data?.data?.is_valid ?? false;
 }
 
