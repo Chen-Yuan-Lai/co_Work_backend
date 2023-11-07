9eff4b682e105c66e6bf311c7b4b374f6a56ffb6
ann <annchung2020@gmail.com>
2023-11-07 16:54:35 +0800

    Add: Complete preorder basic api without checkout

--- server/routes/preorder.ts
+++ server/routes/preorder.ts
@@ -9,66 +9,88 @@ router.use(express.urlencoded({ extended: true }));
 router.get("/new/products/preorder", async (req, res) => {
   console.log("A request came to preorder API");
   let preOrderData = await Preorder.find({});
-  console.log(preOrderData);
 
-  const initialCreatePreorder = new Preorder({
-    id: 1234,
-    category: "preorder",
-    title: "真的是IKEA狗狗",
-    description: "會坐不會站",
-    price: 699,
-    texture: "聚脂纖維",
-    wash: "手洗（水溫40度",
-    place: "韓國",
-    note: "實品顏色以單品照為主",
-    story: "你絕對有看過",
-    target: 69900,
-    accumulate: 60000,
-    progress: "(100/1000)*100",
-    colors: [
-      {
-        code: "D4C2A4",
-        name: "狗狗黃",
-      },
-    ],
-    sizes: ["F"],
-    main_image:
-      "https://www.ikea.com.tw/dairyfarm/tw/images/101/0710164_PE727367_S4.webp",
-    images: [
-      "https://www.ikea.com.tw/dairyfarm/tw/images/768/0876835_PE611246_S4.webp",
-      "https://www.ikea.com.tw/dairyfarm/tw/images/331/0933106_PH167496_S4.webp",
-    ],
+  //   const initialCreatePreorder = new Preorder();
+  //   await initialCreatePreorder.save();
+
+  interface OneResponse {
+    id: any;
+    category: any;
+    title: any;
+    desciption: any;
+    price: any;
+    texture: any;
+    wash: any;
+    place: any;
+    note: any;
+    story: any;
+    target: any;
+    accumulate: any;
+    progress: any;
+    colors: {
+      code?: string | null | undefined;
+      name?: string | null | undefined;
+    }[];
+    sizes: string[];
+    main_image: any;
+    images: string[];
+  }
+
+  interface ResponseData {
+    data: OneResponse[];
+  }
+
+  const response: ResponseData = {
+    data: [],
+  };
+
+  preOrderData.forEach((onePreOrder) => {
+    let target = onePreOrder.target;
+    let accumulate = onePreOrder.accumulate;
+    let progress = null;
+    if (
+      accumulate !== null &&
+      accumulate !== undefined &&
+      target !== null &&
+      target !== undefined
+    ) {
+      progress = (accumulate / target) * 100;
+    }
+
+    let colors: any[] = [];
+    onePreOrder.colors.forEach((onePreOrderColors) => {
+      let oneColor = {
+        code: onePreOrderColors.code,
+        name: onePreOrderColors.name,
+      };
+      colors.push(oneColor);
+    });
+
+    console.log(onePreOrder.colors);
+
+    const oneResponse: OneResponse = {
+      id: onePreOrder.id,
+      category: onePreOrder.category,
+      title: onePreOrder.title,
+      desciption: onePreOrder.desciption,
+      price: onePreOrder.price,
+      texture: onePreOrder.texture,
+      wash: onePreOrder.wash,
+      place: onePreOrder.place,
+      note: onePreOrder.note,
+      story: onePreOrder.story,
+      target: onePreOrder.target,
+      accumulate: onePreOrder.accumulate,
+      progress: progress,
+      colors: colors,
+      sizes: onePreOrder.sizes,
+      main_image: onePreOrder.main_image,
+      images: onePreOrder.images,
+    };
+    response.data.push(oneResponse);
   });
-  await initialCreatePreorder.save();
 
-  //   interface OneResponse {
-  //     sendTime: Date | null | undefined;
-  //     isUser: boolean;
-  //     content: string | null | undefined;
-  //   }
-
-  //   interface ResponseData {
-  //     data: OneResponse[];
-  //   }
-
-  //   const response: ResponseData = {
-  //     data: [],
-  //   };
-
-  //   data.forEach((oneChat) => {
-  //     let isUser: boolean = false;
-  //     if (oneChat.userType == "user") {
-  //       isUser = true;
-  //     }
-  //     const oneResponse: OneResponse = {
-  //       sendTime: oneChat.sendTime,
-  //       isUser: isUser,
-  //       content: oneChat.content,
-  //     };
-  //     response.data.push(oneResponse);
-  //   });
-
-  //   res.status(200).send(response);
+  res.status(200).send(response);
 });
 
 export default router;
